f8247024729cb4b1f15f01abb8c5ac62ce6dec6b
ELDJAZAERY <37628726+ELDJAZAERY@users.noreply.github.com>
2018-07-08 20:48:48 +0100

    Add files via upload

---
+++ Assets/Scripts/spharController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spharController : MonoBehaviour {
+
+    public float jumpForce=50;
+    [Range(100,500)]
+    public float ballSpeed = 500;
+    Rigidbody body;
+
+    bool jumpEnable = true;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+
+        //if(Input.GetKey(KeyCode.RightWindows)) { GetComponent<Rigidbody>().AddForce(50, 0, 0); Debug.Log("Righte !!");  }  
+
+        float xSpeed =  -Input.GetAxis("Vertical");
+        float ySpeed =  Input.GetAxis("Horizontal");
+        //body.AddTorque(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
+        body.AddForce(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpEnable){
+            jumpEnable = false;
+            body.AddForce(new Vector3(xSpeed,jumpForce, ySpeed) * ballSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag == "floor")  jumpEnable = true;
+    }
+
+}
